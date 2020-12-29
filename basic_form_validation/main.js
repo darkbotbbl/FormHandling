ee79d5a7d95bb2c7f7f8a824c2831beb9e29ca34
darkbot <darkbotbbl@gmail.com>
2020-12-29 11:11:01 +0000

    Added basic form validation

--- basic_form_validation/main.js
+++ basic_form_validation/main.js
@@ -1,14 +1,16 @@
 const InputForm = {
 	template: `
 		<div>
-			<form @submit="onFormSubmit">
+			<form @submit="submitForm">
 				<div class="mt-3 form-group">
 					<label>Item</label>
 					<input v-model="fields.newItem" type="text" placeholder="Add an item" class="form-control">
+					<span class="form-text text-danger">{{ fieldErrors.newItem }}</span>
 				</div>
 				<div class="mt-3 form-group">
 					<label>Email</label>
 					<input v-model="fields.email" type="email" placeholder="Enter your email" class="form-control">
+					<span class="form-text text-danger">{{ fieldErrors.email }}</span>
 				</div>
 				<div class="form-group mt-3">
 					<label for="">Urgency</label>
@@ -18,12 +20,14 @@ const InputForm = {
 						<option>Moderate</option>
 						<option>Urgent</option>
 					</select>
+					<span class="form-text text-danger">{{ fieldErrors.urgency }}</span>
 				</div>
 				<div class="form-check">
-				  <label class="form-check-label">
-					<input v-model="fields.termsAndConditions" type="checkbox" class="form-check-input" name="" id="" value="checkedValue" checked>
-					I accept the terms and conditions
-				  </label>
+					<label class="form-check-label">
+						<input v-model="fields.termsAndConditions" type="checkbox" class="form-check-input" name="" id="" value="checkedValue" checked>
+						I accept the terms and conditions
+					</label>
+					<span class="form-text text-danger">{{ fieldErrors.termsAndConditions }}</span>
 				</div>
 				<button type="submit" class="btn btn-success mt-2">Submit</button>
 			</form>
@@ -56,10 +60,35 @@ const InputForm = {
 		}
 	},
 	methods: {
-		onFormSubmit(event) {
+		submitForm(event) {
 			event.preventDefault();
+			
+			this.fieldErrors = this.validateForm(this.fields);
+			if (Object.keys(this.fieldErrors).length) return;
+
 			this.items.push(this.fields.newItem);
 			this.fields.newItem = '';
+			this.fields.email = '';
+			this.fields.urgency = '';
+			this.fields.termsAndConditions = false;
+
+		},
+		validateForm(fields) {
+			const errors = {};
+			if (!fields.newItem) errors.newItem = "Item field required";
+			if (!fields.email) errors.email = "Email field required";
+			if (!fields.urgency) errors.urgency = "Urgency required";
+			if (!fields.termsAndConditions) {
+				errors.termsAndConditions = "Terms and Conditions have to be accepted"
+			}
+			if (fields.email && !this.isEmail(fields.email)) {
+				errors.email = "Invalid email";
+			}
+			return errors;
+		},
+		isEmail(email) {
+			const re = /\S+@\S+.\S+/;
+			return re.test(email);
 		}
 	}
 }
